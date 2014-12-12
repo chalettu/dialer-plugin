3ccf70d29354c0642187aa858428bc82f9988191
Chris Hale <chrishale@CHRHALE-M-D0WK.cisco.com>
2014-12-12 13:46:21 -0600

    updated dialer code

--- www/dialer.js
+++ www/dialer.js
@@ -1,19 +1,6 @@
-var exec = require("cordova/exec");
+var dialNumber =function(phone_number, successCallback, errorCallback) {
 
-
-function DialerPlugin() {}
-
-// **Initialize speech kit**
-//
-// `credentialClassName`  The class name to be loaded to retrieve the app id and key  
-// `serverName`  The hostname of the server to connect to  
-// `port`  The port number for connection  
-// `sslEnabled`  True if SSL is enabled for the connection  
-// `successCallback`  The callback function for success  
-// `failureCallback`  The callback function for error  
-DialerPlugin.prototype.dialNumber =function(phone_number, successCallback, errorCallback) {
-
-  exec(
+  cordova.exec(
       successCallback, // success callback function
       errorCallback, // error callback function
       'DialerPlugin', // mapped to our native Java class called "Calendar"
@@ -26,5 +13,4 @@ DialerPlugin.prototype.dialNumber =function(phone_number, successCallback, error
 
 });
 
- var DialerPlugin = new DialerPlugin();
-        module.exports = DialerPlugin;
+        module.exports = dialNumber;
